640f569e5d6500a1c25e6764c8beb34cfa439d30
Lauren <llwor94@gmail.com>
2019-06-18 20:57:09 -0700

    added links

--- src/components/contact.js
+++ src/components/contact.js
@@ -49,6 +49,9 @@ const ContactBox = styled.div`
   }
   .handle {
     font-size: 16px;
+    cursor: pointer;
+    text-decoration: none;
+    color: ${props => props.theme.white}
   }
   /* &:hover > .details {
     display: flex;
@@ -75,8 +78,8 @@ const ContactBox = styled.div`
 export const Contact = () => {
   return (
     <ContactBox>
-    <div className='contact details'>CONTACT</div>
-    <div style={{display: 'flex', flexGrow: 1, margin: '10px 0 25px'}}>
+    {/* <div className='contact details'>CONTACT</div> */}
+    <div style={{display: 'flex', flexGrow: 1, margin: '25px 0 25px'}}>
       <div className="icons">
         <Twitter />
         <LinkedIn />
@@ -85,11 +88,13 @@ export const Contact = () => {
       <div style={{display: 'flex', flexDirection: 'column'}}>
         
         <div className="details" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', flexGrow: 1}}>
-        <div className='details handle'>@TrevorFehrman</div>
+        <a className='details handle' href="https://twitter.com/TrevorFehrman" target="_blank" rel="noopener noreferrer">@TrevorFehrman</a>
 
-        <div className='details handle'>Trevor Fehrman</div>
+        <a className='details handle' href='https://www.linkedin.com/in/trevor-fehrman-0042656/'
+						target='_blank'
+						rel='noopener noreferrer'>Trevor Fehrman</a>
 
-        <div className='details handle'>trevorfehrman@gmail.com</div>
+        <a className='details handle' href='mailto:trevorfehrman@gmail.com'>trevorfehrman@gmail.com</a>
         </div>
       </div>
       </div>

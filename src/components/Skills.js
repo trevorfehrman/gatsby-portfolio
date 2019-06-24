eda51049bd0f8541ddf4b99a4af58c53f776b68d
Trevor Fehrman <trevorfehrman@gmail.com>
2019-06-23 20:25:52 -0700

    skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,26 +1,85 @@
 import React from "react"
 import SkillBar from "react-skillbars"
-import { skillsData } from "../utils/skillsData"
+import { frontendData, backendData, otherData } from "../utils/skillsData"
 
 import styled from "styled-components"
 
 const SkillContainer = styled.div`
-  width: 60%;
+  width: 85%;
   margin: 10rem auto 0 auto;
   padding-bottom: 3rem;
-`
+  display: flex;
+  justify-content: space-around;
 
-const colors = {
-  bar: "red",
+  & > div {
+    width: 30%;
+  }
+
+  .skillbar {
+    background-color: #272727b5;
+  }
+
+  .skillbar-percent {
+    color: white;
+  }
+
+  .skillbar-bar {
+    transition: width 1000ms ease-out !important;
+  }
+`
+const frontendColors = {
+  bar: {
+    hue: 0,
+    saturation: 0,
+    level: {
+      minimum: 60,
+      maximum: 80,
+    },
+  },
   title: {
     text: "white",
     background: {
-      hue: 100,
-      saturation: 99,
-      level: {
-          minimum: 20,
-          maximum: 100,
-      }
+      hue: 4,
+      saturation: 90,
+      level: 58,
+    },
+  },
+}
+
+const backendColors = {
+  bar: {
+    hue: 0,
+    saturation: 0,
+    level: {
+      minimum: 60,
+      maximum: 80,
+    },
+  },
+  title: {
+    text: "white",
+    background: {
+      hue: 181,
+      saturation: 71,
+      level: 45,
+    },
+  },
+}
+
+const otherColors = {
+  bar: {
+    hue: 0,
+    saturation: 0,
+    level: {
+      minimum: 60,
+      maximum: 80,
+    },
+  },
+  title: {
+    text: "white",
+    background: {
+      hue: 33,
+      saturation: 100,
+      level: 49,
     },
   },
 }
@@ -29,10 +88,28 @@ const Skills = () => {
   return (
     <SkillContainer>
       <SkillBar
-        colors={colors}
+        style={{ width: "30%" }}
+        colors={frontendColors}
         animationDelay={1}
+        animationDuration={1000}
         height={50}
-        skills={skillsData}
+        skills={frontendData}
+      />
+      <SkillBar
+        style={{ width: "30%" }}
+        colors={backendColors}
+        animationDelay={1}
+        animationDuration={1000}
+        height={50}
+        skills={backendData}
+      />
+      <SkillBar
+        style={{ width: "30%" }}
+        colors={otherColors}
+        animationDelay={1}
+        animationDuration={1000}
+        height={50}
+        skills={otherData}
       />
     </SkillContainer>
   )
--- src/utils/skillsData.js
+++ src/utils/skillsData.js
@@ -1,9 +1,28 @@
-export const skillsData = [
+export const frontendData = [
   { type: "React 16.8", level: 90 },
-  { type: "Redux", level: 80 },
-  { type: "CSS in JS", level: 85 },
+  { type: "Redux", level: 85 },
+  { type: "CSS in JS", level: 95 },
   { type: "Next.js", level: 55 },
   { type: "Gatsby", level: 70 },
   { type: "HTML5/CSS3", level: 90 },
   { type: "Material UI", level: 85 },
+]
+
+export const backendData = [
+  { type: "Node", level: 90 },
+  { type: "Express", level: 80 },
+  { type: "SQL", level: 80 },
+  { type: "GraphQL", level: 70 },
+  { type: "Docker", level: 50 },
+  { type: "Auth0", level: 50 },
+]
+
+export const otherData = [
+    {type: "JavaScript", level: 90},
+    {type: "C", level: 60},
+    {type: "Python", level: 75},
+    {type: "Illustrator", level: 40},
+    {type: "Figma/Sketch", level: 60},
+    {type: "Storybook", level: 50},
+
 ]
